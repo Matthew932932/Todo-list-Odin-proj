3358777ef4439163ed35bffe8a02074bba455d16
Matthew932932 <184472697+Matthew932932@users.noreply.github.com>
2024-12-06 18:14:46 +1100

    getting ESLint and Prettier working

--- src/index.js
+++ src/index.js
@@ -1,11 +1,59 @@
-import "./styles.css";
-import odinImage from "./odin-lined.png";
-   
-const image = document.createElement("img");
-image.src = odinImage;
-   
-document.body.appendChild(image);
+import './styles.css'
+import odinImage from './odin-lined.png'
 
+const image = document.createElement('img')
+image.src = odinImage
 
-console.log("heyo");
+document.body.appendChild(image)
 
+console.log('heyo')
+
+const item = new Object()
+
+console.log(item)
+
+const atom = {
+    value: 1,
+
+    addValue: function (value) {
+        return atom.value + value
+    },
+}
+
+const lukeSkywalker = 'Luke Skywalker'
+
+// bad
+const obj = {
+    lukeSkywalker: lukeSkywalker,
+}
+
+// bad
+const bad = {
+    foo: 3,
+    bar: 4,
+    'data-blah': 5,
+}
+
+let myArray = ['one', 'two', 'three']
+
+var indexMap = myArray.reduce(function (memo, item, index) {
+    memo[item] = index
+}, {})
+
+console.log(indexMap)
+
+class B {
+    construtor() {}
+}
+
+class A extends B {
+    constructor() {
+        super()
+    } // Would throw a ReferenceError.
+}
+
+var obj2 = {
+    my_pref: 1,
+}
+
+console.log(obj2)
